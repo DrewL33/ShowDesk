e1d56a9d0b812d747637f82967fdacf7b01a31c7
Drew <65008249+DrewL33@users.noreply.github.com>
2026-09-24 21:36:46 -0500

    Use extracted static asset registry

--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
 const http = require('node:http');
-const path = require('node:path');
-const fs = require('node:fs');
 const { exec } = require('node:child_process');
 const { WebSocketServer } = require('ws');
 let Atem;
@@ -31,17 +29,7 @@ let lastState = null;
 let hasConnected = false;
 const clients = new Set();
 
-function asset(name) {
-  return fs.readFileSync(path.join(__dirname, '..', 'public', name));
-}
-const ASSETS = {
-  '/': ['index.html', 'text/html; charset=utf-8'],
-  '/index.html': ['index.html', 'text/html; charset=utf-8'],
-  '/transport.js': ['transport.js', 'text/javascript; charset=utf-8'],
-  '/signal-paths.js': ['signal-paths.js', 'text/javascript; charset=utf-8'],
-  '/app.js': ['app.js', 'text/javascript; charset=utf-8'],
-  '/styles.css': ['styles.css', 'text/css; charset=utf-8']
-};
+const { asset, ASSETS } = require('./static-assets');
 
 const { normalizeState } = require('./atem-state');
 function discovery(state) {
